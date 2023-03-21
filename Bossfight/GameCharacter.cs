198a596b8f9d2dc528f4256fe15d3ef226b1933b
Åmund Henriksen <aamundhe@getacademy.no>
2023-03-21 17:46:49 +0100

    Add project files.

---
+++ Bossfight/GameCharacter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bossfight
+{
+    public class GameCharacter
+    {
+        public string Name { get; private set; }
+        public int Health { get; private set; }
+        public int Strength { get; private set; }
+        public int Stamina { get; private set; }
+
+        public GameCharacter(string name, int health, int strength, int stamina)
+        {
+            Name = name;
+            Health = health;
+            Strength = strength;
+            Stamina = stamina;
+        }
+
+        public void Fight(GameCharacter enemy)
+        {
+            enemy.Health -= Strength;
+            Stamina -= 10;
+
+            Console.Write($"{Name} hit {enemy.Name} with {Strength} damage, ");
+            Console.WriteLine($"{enemy.Name} has {enemy.Health} health left.");
+        }
+
+        public void Recharge(int staminaAmount)
+        {
+            Console.WriteLine($"{Name} is recharging stamina...");
+            Stamina = staminaAmount;
+        }
+    }
+}
---
+++ Bossfight/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Bossfight
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var hero = new GameCharacter("Hero", 100, 20, 40);
+            var boss = new GameCharacter("Boss", 400, 20, 10);
+
+            while (hero.Health > 0 && boss.Health > 0)
+            {
+                switch (hero.Stamina)
+                {
+                    case <= 0:
+                        hero.Recharge(40);
+                        break;
+                    default:
+                        hero.Fight(boss);
+                        break;
+                }
+
+                switch (boss.Stamina)
+                {
+                    case <= 0:
+                        boss.Recharge(10);
+                        break;
+                    default:
+                        boss.Fight(hero);
+                        break;
+                }
+            }
+
+            if (hero.Health > 0)
+            {
+                WinnerOfTheGame(hero);
+            }
+            else
+            {
+                WinnerOfTheGame(boss);
+            }
+
+            Console.WriteLine("\nPress any key to close the window...");
+            Console.ReadKey();
+        }
+
+        public static void WinnerOfTheGame(GameCharacter character)
+        {
+            Console.WriteLine($"\n{character.Name} is the winner!");
+        }
+    }
+}
